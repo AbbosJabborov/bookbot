cfcb5d7cd77bb3c920b075f1a5f65662593a2c5b
AbbosJabborov <abbosjabborov12@gmail.com>
2025-08-29 17:36:05 +0500

    get book text func

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+path_to_file = "./books/frankenstein.txt"
+
+def get_book_text():
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+get_book_text()
